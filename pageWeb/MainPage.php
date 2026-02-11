b5d58ad0b1e1c50f60dde5d54491d0a6f1a0deaf
justnogame <justnogame.twitch@gmail.com>
2026-02-11 16:35:24 +0100

    Add files via upload

---
+++ pageWeb/MainPage.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site Enigme</title>
+</head>
+<body>
+
+    <table width="100%" border="0">
+        <tr>
+            <td align="left">
+                <a href="classement.html">      <!--lien classement-->
+                    <button style="width:100px; height:50px;"><font size="2">Classement</button>
+                </a>
+            </td>
+
+            <td align="right">
+                <a href="connexion.html">           <!--lien connexion-->
+                    <button style="width:200px; height:50px;"><font size="3">Se connecter</button>
+                </a>
+            </td>
+        </tr>
+    </table> <!-- Bandeau classement et connexion-->
+
+    <hr>
+
+    <h1 align="center">Ceci est le début du site d'énigmes</h1>
+
+    <table width="100%" border="0">
+        <tr>
+            <td align="center">
+            <a href="jouer.html">               <!--lien jouer-->
+                <button style="width:200px; height:50px;"><font size="4">Jouer</button>
+            </a>
+            <br><br>
+
+            <a href="settings.html">            <!--lien settings-->
+                <button style="width:200px; height:50px;"><font size="4">Settings</button>
+            </a>
+            <br><br>
+
+            <a href="credits.html">             <!--lien crédits-->
+                <button style="width:200px; height:50px;"><font size="4">Crédits</button>
+            </a>
+            </td>
+        </tr>
+    </table> <!-- ensemble jouer,classment,crédits-->
+
+</body>
+</html>
